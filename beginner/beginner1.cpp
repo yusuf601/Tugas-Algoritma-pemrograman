f7d606710169f70c0ff804115ad6320f3a1c0976
yusuf601 <yusufmuhyusuh@gmail.com>
2024-09-25 14:03:56 +0700

    fix(Beginner1.cpp): updated solution

--- beginner/beginner1.cpp
+++ beginner/beginner1.cpp
@@ -11,29 +11,13 @@ int keliling(int b){
 
 int main(){
     int s,result,hasil;
-    int pil;
-    bool choice = true;
-    while(choice){
-        cout << "Masukkan sisi persegi: ";
-        cin >> s;
-        result = luas(s);
-        hasil = keliling(s);
-        cout << "Luas persegi: " << s << " x " << s << " = " << result << " \n";
-        cout << "Keliling Persegi: " << " 4 " << " x " << s << " = " << hasil << endl;
-        cout <<  "**************************************************************** \n";
-        cout << "Pilihan yang tersedia \n";
-        cout << "1.ya \n";
-        cout << "2.tidak \n";
-        cout << "Apakah anda ingin melanjutkan program ini?";
-        cin >> pil;
-        while(pil < 1 || pil > 2){ //tambahkan logika OR untuk mengecek nilai pil apakah kurang dari 1 atau lebih dari 2
-            cout << "Masukkan input lagi! ";
-            cin >> pil;
-       }
-       if(pil == 1){
-        choice = true;
-       }else if(pil == 2){
-        break;
-       }
-    }
+
+    cout << "Masukkan sisi persegi: ";
+    cin >> s;
+    result = luas(s);
+    hasil = keliling(s);
+    cout << "Luas persegi: " << s << " x " << s << " = " << result << " \n";
+    cout << "Keliling Persegi: " << " 4 " << " x " << s << " = " << hasil << endl;
+  
+    return 0;
 }
